9d4ef32857cdc8e8cb0fb497ee55acb543011d87
Hector Alpizar <hector.alpizar.c@gmail.com>
2023-10-01 09:58:13 -0700

    Build Configuration flags added

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -2,6 +2,8 @@
 
 
 #include "PositionReporter.h"
+#include "BuildingEscape.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
--- Source/BuildingEscapeEditor.Target.cs
+++ Source/BuildingEscapeEditor.Target.cs
@@ -10,5 +10,8 @@ public class BuildingEscapeEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "BuildingEscape" } );
-	}
+
+        bUseUnityBuild = false;
+        bUsePCHFiles = false;
+    }
 }
