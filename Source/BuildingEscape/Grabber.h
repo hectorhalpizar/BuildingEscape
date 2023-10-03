6745c2d2b41c3649100d39e3edde868428b777bd
Hector Alpizar <hector.alpizar.c@gmail.com>
2023-10-02 23:59:28 -0700

    Input binding added to grabber

--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -33,5 +33,10 @@ private:
 	float Reach = 100.f;
 
 	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
+	UInputComponent* InputComponent = nullptr;
+
+	// Ray-cast and grab what's in reach
+	void Grab();
 		
 };
